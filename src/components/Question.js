3fd48fed1168fc57f83da22749d5790dcb52ddaa
Mitchell <Hmitchell.hall@gmail.com>
2021-09-23 14:38:15 -0500

    commit tree e7d132e0b42dacc3d4f57d9bb525c5ccee0c1869

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if(timeRemaining === 0){
+        clearTimeout(timer)
+        setTimeRemaining(10)
+        onAnswered(false) 
+      }
+      else setTimeRemaining(timeRemaining -1);
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
